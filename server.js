f867285d92fd1f586143e90efa7a605c0667249e
Luke <ldary@redhat.com>
2022-09-05 20:41:03 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+import { createApp, serveStatic } from "https://deno.land/x/servest@v1.3.1/mod.ts";
+const app = createApp();
+// All requests will be processed and matched files in "public" directory
+// are served automatically
+// Otherwise, request will be passed to next handler
+app.use(serveStatic("./public"));
+app.listen({ port: 8080 });
